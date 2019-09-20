96c9438b86a929d6c30afefc20d941b9591e3d46
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-20 08:51:20 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -7,7 +7,8 @@ age INTEGER
 CREATE TABLE projects (
   id INTEGER PRIMARY KEY,
   title TEXT, 
-  category, funding_goal, start_date, end_date
+  category TEXT, 
+  funding_goal TEXT, start_date, end_date
 ); 
 
 CREATE TABLE pledges (
