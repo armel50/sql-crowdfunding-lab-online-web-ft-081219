a5b6cc3fcfe110b2028b814ca09653e0dc762935
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-20 09:25:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age ,pledges.amount FROM users 
     INNER JOIN pledges ON users.id = pledges.user_id  
-    ORDER BY user.name
+    ORDER BY users.name
     
   "
 end
