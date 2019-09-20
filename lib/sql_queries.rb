eb019134ac195fb4145e9b40b73cab4e18523e86
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-20 09:39:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -28,7 +28,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, projects.funding_goal - pledges.amount FROM projects
   INNER JOIN pledges ON projects.id = pledges.project_id 
-  WHERE pledges.amount - projects.funding_goal = 0;
+  WHERE pledges.amount - projects.funding_goal = 0 OR pledges.amount > projects.funding_goal;
   "
 end
 
