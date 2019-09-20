e08ae639545ccd46f52c1920466128fa10b05038
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-20 08:59:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,7 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT title FROM projects 
-    INNER JOIN pleges on title.
+    INNER JOIN pleges on projects.id = pleges.projects_id 
   "
 end
 
